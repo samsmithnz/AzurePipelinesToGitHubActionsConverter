a59bccc78399104dffad967ea06ca1e80e451e4b
Sam Smith <samsmithnz@gmail.com>
2019-11-03 21:04:26 -0500

    Added new processing for steps

--- AzurePipelinesToGitHubActionsConverter/AzurePipelinesToGitHubActionsConverter.Core/GitHubActions/Step.cs
+++ AzurePipelinesToGitHubActionsConverter/AzurePipelinesToGitHubActionsConverter.Core/GitHubActions/Step.cs
@@ -9,5 +9,7 @@ namespace AzurePipelinesToGitHubActionsConverter.Core.GitHubActions
         public string run { get; set; }
         public string shell { get; set; }
         public Dictionary<string, string> with { get; set; } //A key value pair similar to env
+        public Dictionary<string, string> env { get; set; } //Similar to the job env: https://help.github.com/en/articles/workflow-syntax-for-github-actions#jobsjob_idenv
+
     }
 }
