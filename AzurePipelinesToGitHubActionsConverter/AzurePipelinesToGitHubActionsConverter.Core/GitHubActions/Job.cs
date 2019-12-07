1032061afb874d3f61527209d9cc35b404328e1f
Sam Smith (Microsoft) <samsmithnz@gmail.com>
2019-12-07 10:06:52 -0500

    More enhancements part 7 (#53)
    * Created PowerShell file for versioning, and increased version to 0.7
    
    * Update CI.yml
    
    Trying a PowerShell file instead of inline script
    
    * Fixed typo in build version script
    
    * Tweaks to streamline powershell output
    
    * Final build script numbers
    
    * Back up to 96% code coverage

--- AzurePipelinesToGitHubActionsConverter/AzurePipelinesToGitHubActionsConverter.Core/GitHubActions/Job.cs
+++ AzurePipelinesToGitHubActionsConverter/AzurePipelinesToGitHubActionsConverter.Core/GitHubActions/Job.cs
@@ -7,7 +7,9 @@ namespace AzurePipelinesToGitHubActionsConverter.Core.GitHubActions
         public string name { get; set; } //https://help.github.com/en/articles/workflow-syntax-for-github-actions#jobsjob_idname
         public string runs_on { get; set; } //https://help.github.com/en/articles/workflow-syntax-for-github-actions#jobsjob_idruns-on
         public Strategy strategy { get; set; } //https://help.github.com/en/articles/workflow-syntax-for-github-actions#jobsjob_idstrategy
+        //public T container { get; set; }
         public Container container { get; set; } //https://docs.microsoft.com/en-us/azure/devops/pipelines/yaml-schema?view=azure-devops&tabs=schema#job
+        //public string container { get; set; } //https://docs.microsoft.com/en-us/azure/devops/pipelines/yaml-schema?view=azure-devops&tabs=schema#job
         public string timeout_minutes { get; set; } //https://help.github.com/en/articles/workflow-syntax-for-github-actions#jobsjob_idtimeout-minutes
         public string needs { get; set; } //https://help.github.com/en/articles/workflow-syntax-for-github-actions#jobsjob_idneeds
         public Dictionary<string, string> env { get; set; } //https://help.github.com/en/articles/workflow-syntax-for-github-actions#jobsjob_idenv
---
+++ AzurePipelinesToGitHubActionsConverter/BuildVersion.ps1
@@ -0,0 +1,27 @@
+param
+(
+	[string] $ProjectFile
+)
+
+Write-Host "Generating Build Number"
+#Get the version from the csproj file
+$xml = [Xml] (Get-Content $ProjectFile)
+$initialVersion = [Version] $xml.Project.PropertyGroup.Version
+Write-Host "Initial version read from project file: $initialVersion" 
+$splitVersion = $initialVersion -Split "\."
+#Get the build number (number of days since January 1, 2000)
+$baseDate = [datetime]"01/01/2000"
+$currentDate = $(Get-Date)
+$interval = (NEW-TIMESPAN -Start $baseDate -End $currentDate)
+$buildNumber = $interval.Days
+#Get the revision number (number seconds (divided by two) into the day on which the compilation was performed)
+$StartDate=[datetime]::Today
+$EndDate=(GET-DATE)
+$revisionNumber = [math]::Round((New-TimeSpan -Start $StartDate -End $EndDate).TotalSeconds / 2,0)
+#Final version number, using the Major, Minor and Patch versions, and then appends build and revision number together
+$finalBuildVersion = "$($splitVersion[0]).$($splitVersion[1]).$($splitVersion[2]).$($buildNumber)$($revisionNumber)"
+#Write-Host "Major.Minor,patch,Build+Revision"
+Write-Host "Final build number: $finalBuildVersion" 
+#Writing final version number back to Github variable
+#Write-Host "Writing final version $finalBuildVersion back to Github variable"
+echo "::set-env name=buildVersion::$finalBuildVersion"
