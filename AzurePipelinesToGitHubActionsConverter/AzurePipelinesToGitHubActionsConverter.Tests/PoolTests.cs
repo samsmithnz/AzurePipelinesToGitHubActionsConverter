3e0aa2ce90d2a174aaa00cb7b29c86868884cc1e
Sam Smith <samsmithnz@gmail.com>
2020-08-03 10:39:12 -0400

    Addess the multiple variations of pools

--- AzurePipelinesToGitHubActionsConverter/AzurePipelinesToGitHubActionsConverter.Tests/PoolTests.cs
+++ AzurePipelinesToGitHubActionsConverter/AzurePipelinesToGitHubActionsConverter.Tests/PoolTests.cs
@@ -7,6 +7,15 @@ using System;
 
 namespace AzurePipelinesToGitHubActionsConverter.Tests
 {
+    //pool can be either:
+    //pool:
+    //  name: string  # name of the pool to run this job in
+    //  demands: string | [ string ]  # see the following "Demands" topic
+    //  vmImage: string # name of the VM image you want to use; valid only in the Microsoft-hosted pool
+
+    //OR:
+    //pool: string # name of the private pool to run this job in
+
     [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
     [TestClass]
     public class PoolTests
@@ -76,29 +85,49 @@ jobs:
             Assert.AreEqual(expected, gitHubOutput.actionsYaml);
         }
 
-//        [TestMethod]
-//        public void PoolNameDependsListStringTest()
-//        {
-//            //Arrange
-//            string input = @"
-//pool:
-//  name: Hosted VS2017
-//  demands: 
-//  - npm";
-//            Conversion conversion = new Conversion();
+        [TestMethod]
+        public void PoolNameDependsListStringTest()
+        {
+            //Arrange
+            string input = @"
+pool:
+  name: Hosted VS2017
+  demands: 
+  - npm";
+            Conversion conversion = new Conversion();
 
-//            //Act
-//            ConversionResponse gitHubOutput = conversion.ConvertAzurePipelineToGitHubAction(input);
+            //Act
+            ConversionResponse gitHubOutput = conversion.ConvertAzurePipelineToGitHubAction(input);
 
-//            //Assert
-//            string expected = @"
-//#Note: GitHub Actions does not have a 'demands' command on 'runs-on' yet
-//jobs:
-//  build:
-//    runs-on: Hosted VS2017";
-//            expected = UtilityTests.TrimNewLines(expected);
-//            Assert.AreEqual(expected, gitHubOutput.actionsYaml);
-//        }
+            //Assert
+            string expected = @"
+#Note: GitHub Actions does not have a 'demands' command on 'runs-on' yet
+jobs:
+  build:
+    runs-on: Hosted VS2017";
+            expected = UtilityTests.TrimNewLines(expected);
+            Assert.AreEqual(expected, gitHubOutput.actionsYaml);
+        }
+
+        [TestMethod]
+        public void PoolSimpleNameTest()
+        {
+            //Arrange
+            string input = @"
+pool: windows-latest";
+            Conversion conversion = new Conversion();
+
+            //Act
+            ConversionResponse gitHubOutput = conversion.ConvertAzurePipelineToGitHubAction(input);
+
+            //Assert
+            string expected = @"
+jobs:
+  build:
+    runs-on: windows-latest";
+            expected = UtilityTests.TrimNewLines(expected);
+            Assert.AreEqual(expected, gitHubOutput.actionsYaml);
+        }
 
     }
 }
