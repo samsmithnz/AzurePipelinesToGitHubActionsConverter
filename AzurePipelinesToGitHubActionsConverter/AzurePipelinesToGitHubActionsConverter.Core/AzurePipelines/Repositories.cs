68ce5f159a60c93b0fb468403cb31d8e8b0d0727
Sam Smith <samsmithnz@gmail.com>
2020-08-07 12:03:57 -0400

    Resolving items

--- AzurePipelinesToGitHubActionsConverter/AzurePipelinesToGitHubActionsConverter.Core/AzurePipelines/Repositories.cs
+++ AzurePipelinesToGitHubActionsConverter/AzurePipelinesToGitHubActionsConverter.Core/AzurePipelines/Repositories.cs
@@ -4,12 +4,12 @@ using System.Text;
 
 namespace AzurePipelinesToGitHubActionsConverter.Core.AzurePipelines
 {
-  //repositories:
-  //- repository: string  # identifier (A-Z, a-z, 0-9, and underscore)
-  //  type: enum  # see below
-  //  name: string  # repository name (format Demands on `type`)
-  //  ref: string  # ref name to use, defaults to 'refs/heads/master'
-  //  endpoint: string  # name of the service connection to use (for non-Azure Repos types)
+    //repositories:
+    //- repository: string  # identifier (A-Z, a-z, 0-9, and underscore)
+    //  type: enum  # see below
+    //  name: string  # repository name (format depends on `type`)
+    //  ref: string  # ref name to use, defaults to 'refs/heads/master'
+    //  endpoint: string  # name of the service connection to use (for non-Azure Repos types)
     public class Repositories
     {
         //TODO: Add code to process repositories
--- AzurePipelinesToGitHubActionsConverter/AzurePipelinesToGitHubActionsConverter.Core/AzurePipelines/Stage.cs
+++ AzurePipelinesToGitHubActionsConverter/AzurePipelinesToGitHubActionsConverter.Core/AzurePipelines/Stage.cs
@@ -6,7 +6,7 @@ namespace AzurePipelinesToGitHubActionsConverter.Core.AzurePipelines
         //stages:
         //- stage: Build
         //  displayName: 'Build/Test Stage'
-        //  DemandsOn: PreBuild
+        //  dependsOn: PreBuild
         //  jobs:
         //  - job: Build
         //    displayName: 'Build job'
@@ -15,8 +15,8 @@ namespace AzurePipelinesToGitHubActionsConverter.Core.AzurePipelines
         //    steps:
         public string stage { get; set; }
         public string displayName { get; set; }
-        //Add Demandson processing for stages
-        public string DemandsOn { get; set; }
+        //Add dependsOn processing for stages
+        public string dependsOn { get; set; }
         public string condition { get; set; }
         //Variables is similar to triggers, this can be a simple list, or a more complex variable object
         public List<Variable> variables { get; set; }
