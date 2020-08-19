95cd14cf8d75b4de6047f867efeffe8c7e64c2c9
Sam Smith (Microsoft) <samsmithnz@gmail.com>
2020-08-18 22:08:11 -0400

    Update BuildVersion.ps1
    Removed debug code

--- src/BuildVersion.ps1
+++ src/BuildVersion.ps1
@@ -16,13 +16,13 @@ $baseDate = [datetime]"01/01/2000"
 $currentDate = $(Get-Date)
 $interval = (NEW-TIMESPAN -Start $baseDate -End $currentDate)
 $buildNumber = $interval.Days
-Write-Host $buildNumber
+#Write-Host $buildNumber
 
 #Get the revision number (number seconds (divided by two) into the day on which the compilation was performed)
 $StartDate=[datetime]::Today
 $EndDate=(GET-DATE)
 $revisionNumber = [math]::Round((New-TimeSpan -Start $StartDate -End $EndDate).TotalSeconds / 2,0)
-Write-Host $revisionNumber
+#Write-Host $revisionNumber
 
 #Final version number, using the Major, Minor and Patch versions, and then appends build and revision number together
 $finalBuildVersion = "$($splitVersion[0]).$($splitVersion[1]).$($splitVersion[2]).$($buildNumber)$($revisionNumber.ToString("00000"))"
