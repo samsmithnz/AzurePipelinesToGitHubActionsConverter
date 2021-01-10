d95e3491b0009854c969932f95e2c2dfd6c68047
Sam Smith <samsmithnz@gmail.com>
2021-01-10 09:57:34 -0500

    Cleaned up serialization classes

--- src/AzurePipelinesToGitHubActionsConverter.Core/Serialization/JSONSerialization.cs
+++ src/AzurePipelinesToGitHubActionsConverter.Core/Serialization/JSONSerialization.cs
@@ -6,37 +6,26 @@ namespace AzurePipelinesToGitHubActionsConverter.Core.Serialization
 {
     public static class JsonSerialization
     {
-        //public static JObject DeserializeStringToObject(string yaml)
-        //{
-        //    StringWriter sw = new StringWriter();
-        //    StringReader sr = new StringReader(yaml);
-        //    Deserializer deserializer = new Deserializer();
-        //    var yamlObject = deserializer.Deserialize(sr);
-        //    JsonSerializer serializer = new JsonSerializer();
-        //    serializer.Serialize(sw, yamlObject);
-        //    return JsonConvert.DeserializeObject<JObject>(sw.ToString());
-        //}
-
         public static JsonElement DeserializeStringToJsonElement(string yaml)
         {
+            //This looks inefficient, but helps to ensure the Yaml is clean and well formed
+            //first we convert the yaml to a object 
             StringReader sr = new StringReader(yaml);
             Deserializer deserializer = new Deserializer();
             var yamlObject = deserializer.Deserialize(sr);
+            //then convert the object back to yaml again to format the yaml
             string processedYaml = JsonSerializer.Serialize(yamlObject);
+            //Finally we can return a JsonElement object from the processed Yaml. 
             return JsonSerializer.Deserialize<JsonElement>(processedYaml);
         }
-
-        //public static T DeserializeStringToObject<T>(string json)
-        //{
-        //    return JsonSerializer.Deserialize<T>(json);
-        //}
 
         //From: https://stackoverflow.com/a/36125258/337421
         public static bool JsonCompare(this object obj, object another)
         {
+            //Serialize two objects as strings
             string objJson = JsonSerializer.Serialize(obj);
             string anotherJson = JsonSerializer.Serialize(another);
-
+            //Compare the strings
             return objJson == anotherJson;
         }
     }
