559b8641e77bdc1db68f26ed649bd3b5484cfb69
Sam Smith <samsmithnz@gmail.com>
2021-01-10 10:30:17 -0500

    Cleaned up strategy

--- src/AzurePipelinesToGitHubActionsConverter.Core/PipelinesToActionsConversion/StrategyProcessing.cs
+++ src/AzurePipelinesToGitHubActionsConverter.Core/PipelinesToActionsConversion/StrategyProcessing.cs
@@ -23,28 +23,28 @@ namespace AzurePipelinesToGitHubActionsConverter.Core.PipelinesToActionsConversi
                 AzurePipelines.Strategy newStrategy = new AzurePipelines.Strategy();
                 if (strategyJson.TryGetProperty("parallel", out jsonElement) == true)
                 {
-                    newStrategy.parallel = strategyJson.GetProperty("parallel").ToString();
+                    newStrategy.parallel = jsonElement.ToString();
                     ConversionUtility.WriteLine("Note that Parallel doesn't currently have an equivalent in Actions: " + newStrategy.parallel, _verbose);
                 }
                 if (strategyJson.TryGetProperty("matrix", out jsonElement) == true)
                 {
-                    newStrategy.matrix = YamlSerialization.DeserializeYaml<Dictionary<string, Dictionary<string, string>>>(strategyJson.GetProperty("matrix").ToString());
+                    newStrategy.matrix = YamlSerialization.DeserializeYaml<Dictionary<string, Dictionary<string, string>>>(jsonElement.ToString());
                 }
                 if (strategyJson.TryGetProperty("maxParallel", out jsonElement) == true)
                 {
-                    newStrategy.maxParallel = strategyJson.GetProperty("maxParallel").ToString();
+                    newStrategy.maxParallel = jsonElement.ToString();
                 }
                 if (strategyJson.TryGetProperty("runOnce", out jsonElement) == true)
                 {
-                    newStrategy.runOnce = ProcessRunOnceStrategy(strategyJson.GetProperty("runOnce"));
+                    newStrategy.runOnce = ProcessRunOnceStrategy(jsonElement);
                 }
                 if (strategyJson.TryGetProperty("canary", out jsonElement) == true)
                 {
-                    newStrategy.canary = ProcessCanaryStrategy(strategyJson.GetProperty("canary"));
+                    newStrategy.canary = ProcessCanaryStrategy(jsonElement);
                 }
                 if (strategyJson.TryGetProperty("rolling", out jsonElement) == true)
                 {
-                    newStrategy.rolling = ProcessRollingStrategy(strategyJson.GetProperty("rolling"));
+                    newStrategy.rolling = ProcessRollingStrategy(jsonElement);
                 }
                 return newStrategy;
             }
@@ -60,26 +60,26 @@ namespace AzurePipelinesToGitHubActionsConverter.Core.PipelinesToActionsConversi
             RunOnce runOnce = new RunOnce();
             if (strategyJson.TryGetProperty("preDeploy", out jsonElement) == true)
             {
-                runOnce.preDeploy = ProcessDeploy(strategyJson.GetProperty("preDeploy"));
+                runOnce.preDeploy = ProcessDeploy(jsonElement);
                 ConversionUtility.WriteLine("Note that RunOnce>preDeploy isn't currently processed: " + runOnce.preDeploy.ToString(), _verbose);
             }
             if (strategyJson.TryGetProperty("deploy", out jsonElement) == true)
             {
-                runOnce.deploy = ProcessDeploy(strategyJson.GetProperty("deploy"));
+                runOnce.deploy = ProcessDeploy(jsonElement);
             }
             if (strategyJson.TryGetProperty("routeTraffic", out jsonElement) == true)
             {
-                runOnce.routeTraffic = ProcessDeploy(strategyJson.GetProperty("routeTraffic"));
+                runOnce.routeTraffic = ProcessDeploy(jsonElement);
                 ConversionUtility.WriteLine("Note that RunOnce>preDeploy isn't currently processed: " + runOnce.routeTraffic.ToString(), _verbose);
             }
             if (strategyJson.TryGetProperty("postRouteTraffic", out jsonElement) == true)
             {
-                runOnce.postRouteTraffic = ProcessDeploy(strategyJson.GetProperty("postRouteTraffic"));
+                runOnce.postRouteTraffic = ProcessDeploy(jsonElement);
                 ConversionUtility.WriteLine("Note that RunOnce>preDeploy isn't currently processed: " + runOnce.postRouteTraffic.ToString(), _verbose);
             }
             if (strategyJson.TryGetProperty("on", out jsonElement) == true)
             {
-                runOnce.on = ProcessOn(strategyJson.GetProperty("on"));
+                runOnce.on = ProcessOn(jsonElement);
                 ConversionUtility.WriteLine("Note that RunOnce>on isn't currently processed: " + runOnce.on.ToString(), _verbose);
             }
             return runOnce;
@@ -91,31 +91,31 @@ namespace AzurePipelinesToGitHubActionsConverter.Core.PipelinesToActionsConversi
             Canary canary = new Canary();
             if (strategyJson.TryGetProperty("increments", out jsonElement) == true)
             {
-                canary.increments = YamlSerialization.DeserializeYaml<int[]>(strategyJson.GetProperty("increments").ToString());
+                canary.increments = YamlSerialization.DeserializeYaml<int[]>(jsonElement.ToString());
                 ConversionUtility.WriteLine("Note that canary>increments isn't currently processed: " + canary.increments.ToString(), _verbose);
             }
             if (strategyJson.TryGetProperty("preDeploy", out jsonElement) == true)
             {
-                canary.preDeploy = ProcessDeploy(strategyJson.GetProperty("preDeploy"));
+                canary.preDeploy = ProcessDeploy(jsonElement);
                 ConversionUtility.WriteLine("Note that canary>preDeploy isn't currently processed: " + canary.preDeploy.ToString(), _verbose);
             }
             if (strategyJson.TryGetProperty("deploy", out jsonElement) == true)
             {
-                canary.deploy = ProcessDeploy(strategyJson.GetProperty("deploy"));
+                canary.deploy = ProcessDeploy(jsonElement);
             }
             if (strategyJson.TryGetProperty("routeTraffic", out jsonElement) == true)
             {
-                canary.routeTraffic = ProcessDeploy(strategyJson.GetProperty("routeTraffic"));
+                canary.routeTraffic = ProcessDeploy(jsonElement);
                 ConversionUtility.WriteLine("Note that canary>routeTraffic isn't currently processed: " + canary.routeTraffic.ToString(), _verbose);
             }
             if (strategyJson.TryGetProperty("postRouteTraffic", out jsonElement) == true)
             {
-                canary.postRouteTraffic = ProcessDeploy(strategyJson.GetProperty("postRouteTraffic"));
+                canary.postRouteTraffic = ProcessDeploy(jsonElement);
                 ConversionUtility.WriteLine("Note that canary>postRouteTraffic isn't currently processed: " + canary.postRouteTraffic.ToString(), _verbose);
             }
             if (strategyJson.TryGetProperty("on", out jsonElement) == true)
             {
-                canary.on = ProcessOn(strategyJson.GetProperty("on"));
+                canary.on = ProcessOn(jsonElement);
                 ConversionUtility.WriteLine("Note that canary>on isn't currently processed: " + canary.on.ToString(), _verbose);
             }
             return canary;
@@ -127,7 +127,7 @@ namespace AzurePipelinesToGitHubActionsConverter.Core.PipelinesToActionsConversi
             Rolling rolling = new Rolling();
             if (strategyJson.TryGetProperty("maxParallel", out jsonElement) == true)
             {
-                if (int.TryParse(strategyJson.GetProperty("maxParallel").ToString(), out int maxParallel) == true)
+                if (int.TryParse(jsonElement.ToString(), out int maxParallel) == true)
                 {
                     rolling.maxParallel = maxParallel;
                     ConversionUtility.WriteLine("Note that rolling>maxParallel isn't currently processed: " + rolling.maxParallel.ToString(), _verbose);
@@ -135,26 +135,26 @@ namespace AzurePipelinesToGitHubActionsConverter.Core.PipelinesToActionsConversi
             }
             if (strategyJson.TryGetProperty("preDeploy", out jsonElement) == true)
             {
-                rolling.preDeploy = ProcessDeploy(strategyJson.GetProperty("preDeploy"));
+                rolling.preDeploy = ProcessDeploy(jsonElement);
                 ConversionUtility.WriteLine("Note that rolling>preDeploy isn't currently processed: " + rolling.preDeploy.ToString(), _verbose);
             }
             if (strategyJson.TryGetProperty("deploy", out jsonElement) == true)
             {
-                rolling.deploy = ProcessDeploy(strategyJson.GetProperty("deploy"));
+                rolling.deploy = ProcessDeploy(jsonElement);
             }
             if (strategyJson.TryGetProperty("routeTraffic", out jsonElement) == true)
             {
-                rolling.routeTraffic = ProcessDeploy(strategyJson.GetProperty("routeTraffic"));
+                rolling.routeTraffic = ProcessDeploy(jsonElement);
                 ConversionUtility.WriteLine("Note that rolling>routeTraffic isn't currently processed: " + rolling.routeTraffic.ToString(), _verbose);
             }
             if (strategyJson.TryGetProperty("postRouteTraffic", out jsonElement) == true)
             {
-                rolling.postRouteTraffic = ProcessDeploy(strategyJson.GetProperty("postRouteTraffic"));
+                rolling.postRouteTraffic = ProcessDeploy(jsonElement);
                 ConversionUtility.WriteLine("Note that rolling>postRouteTraffic isn't currently processed: " + rolling.postRouteTraffic.ToString(), _verbose);
             }
             if (strategyJson.TryGetProperty("on", out jsonElement) == true)
             {
-                rolling.on = ProcessOn(strategyJson.GetProperty("on"));
+                rolling.on = ProcessOn(jsonElement);
                 ConversionUtility.WriteLine("Note that rolling>on isn't currently processed: " + rolling.on.ToString(), _verbose);
             }
 
@@ -168,18 +168,11 @@ namespace AzurePipelinesToGitHubActionsConverter.Core.PipelinesToActionsConversi
             Deploy deploy = new Deploy();
             if (deployJson.TryGetProperty("pool", out jsonElement) == true)
             {
-                deploy.pool = gp.ProcessPoolV2(deployJson.GetProperty("pool").ToString());
+                deploy.pool = gp.ProcessPoolV2(jsonElement.ToString());
             }
             if (deployJson.TryGetProperty("steps", out jsonElement) == true)
             {
-                //try
-                //{
-                deploy.steps = YamlSerialization.DeserializeYaml<AzurePipelines.Step[]>(deployJson.GetProperty("steps").ToString());
-                //}
-                //catch (Exception ex)
-                //{
-                //    ConversionUtility.WriteLine($"DeserializeYaml<AzurePipelines.Step[]>(ProcessDeploy[\"steps\"].ToString() swallowed an exception: " + ex.Message, _verbose);
-                //}
+                deploy.steps = YamlSerialization.DeserializeYaml<AzurePipelines.Step[]>(jsonElement.ToString());
             }
 
             return deploy;
@@ -191,12 +184,12 @@ namespace AzurePipelinesToGitHubActionsConverter.Core.PipelinesToActionsConversi
             On on = new On();
             if (onJson.TryGetProperty("success", out jsonElement) == true)
             {
-                on.success = ProcessDeploy(onJson.GetProperty("success"));
+                on.success = ProcessDeploy(jsonElement);
                 ConversionUtility.WriteLine("Note that success isn't currently processed: " + on.success.ToString(), _verbose);
             }
             if (onJson.TryGetProperty("failure", out jsonElement) == true)
             {
-                on.failure = ProcessDeploy(onJson.GetProperty("failure"));
+                on.failure = ProcessDeploy(jsonElement);
                 ConversionUtility.WriteLine("Note that failure isn't currently processed: " + on.failure.ToString(), _verbose);
             }
             return on;
