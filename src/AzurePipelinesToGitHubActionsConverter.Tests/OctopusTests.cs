990bf837af79dd9abc61d23bb3492060b3647726
Sam Smith <samsmithnz@gmail.com>
2021-12-08 12:49:57 -0500

    Added data into octopus tests

--- src/AzurePipelinesToGitHubActionsConverter.Tests/OctopusTests.cs
+++ src/AzurePipelinesToGitHubActionsConverter.Tests/OctopusTests.cs
@@ -14,15 +14,14 @@ namespace AzurePipelinesToGitHubActionsConverter.Tests
             //Arrange
             Conversion conversion = new Conversion();
             string yaml = @"
-- task: Ant@1
+- task: octopusdeploy.octopus-deploy-build-release-tasks.octopus-pack.OctopusPack@4
+  displayName: 'Package OctopusSamples.OctoPetShop.Database'
   inputs:
-    workingDirectory: ''
-    buildFile: 'build.xml'
-    javaHomeOption: 'JDKVersion'
-    jdkVersionOption: '1.8'
-    jdkArchitectureOption: 'x64'
-    publishJUnitResults: true
-    testResultsFiles: '**/TEST-*.xml'  
+    PackageId: OctopusSamples.OctoPetShop.Database
+    PackageFormat: Zip
+    PackageVersion: '$(Build.BuildNumber)'
+    SourcePath: '$(build.artifactstagingdirectory)\output\OctoPetShop.Database\'
+    OutputPath: '$(Build.SourcesDirectory)\output'
 ";
 
             //Act
@@ -30,7 +29,9 @@ namespace AzurePipelinesToGitHubActionsConverter.Tests
 
             //Assert
             string expected = @"
-
+    - name: Package OctoPetShopDatabase
+      run: |
+        octo pack --id=""OctoPetShop.Database"" --format=""Zip"" --version=""$PACKAGE_VERSION"" --basePath=""$GITHUB_WORKSPACE/artifacts/OctopusSamples.OctoPetShop.Database"" --outFolder=""$GITHUB_WORKSPACE/artifacts""
 ";
             expected = UtilityTests.TrimNewLines(expected);
             Assert.AreEqual(expected, gitHubOutput.actionsYaml);
@@ -42,15 +43,14 @@ namespace AzurePipelinesToGitHubActionsConverter.Tests
             //Arrange
             Conversion conversion = new Conversion();
             string yaml = @"
-- task: Ant@1
+- task: octopusdeploy.octopus-deploy-build-release-tasks.octopus-pack.OctopusPack@4
+  displayName: 'Package OctopusSamples.Octopetshop.ShoppingCartService'
   inputs:
-    workingDirectory: ''
-    buildFile: 'build.xml'
-    javaHomeOption: 'JDKVersion'
-    jdkVersionOption: '1.8'
-    jdkArchitectureOption: 'x64'
-    publishJUnitResults: true
-    testResultsFiles: '**/TEST-*.xml'  
+    PackageId: OctopusSamples.OctoPetShop.ShoppingCartService
+    PackageFormat: Zip
+    PackageVersion: '$(Build.BuildNumber)'
+    SourcePath: '$(build.artifactstagingdirectory)\output\OctoPetShop.ShoppingCartService\'
+    OutputPath: '$(Build.SourcesDirectory)\output' 
 ";
 
             //Act
@@ -58,7 +58,9 @@ namespace AzurePipelinesToGitHubActionsConverter.Tests
 
             //Assert
             string expected = @"
-
+- name: Push OctoPetShop Database
+  run: |
+    octo push --package=""$GITHUB_WORKSPACE/artifacts/OctoPetShop.Database.$PACKAGE_VERSION.zip"" --server=""${{ secrets.OCTOPUSSERVERURL }}"" --apiKey=""${{ secrets.OCTOPUSSERVERAPIKEY }}"" --space=""${{ secrets.OCTOPUSSERVER_SPACE }}""
 ";
             expected = UtilityTests.TrimNewLines(expected);
             Assert.AreEqual(expected, gitHubOutput.actionsYaml);
@@ -69,7 +71,129 @@ namespace AzurePipelinesToGitHubActionsConverter.Tests
         {           
             //Arrange
             Conversion conversion = new Conversion();
-            string yaml = "";
+            string yaml = @"
+# ASP.NET Core (.NET Framework)
+# Build and test ASP.NET Core projects targeting the full .NET Framework.
+# Add steps that publish symbols, save build artifacts, and more:
+# https://docs.microsoft.com/azure/devops/pipelines/languages/dotnet-core
+
+trigger:
+- master
+
+pool:
+  vmImage: 'windows-2019'
+  demands:
+  - msbuild
+  - visualstudio
+
+variables:
+  solution: '**/*.sln'
+  buildPlatform: 'Any CPU'
+  buildConfiguration: 'Release'
+
+steps:
+- task: DotNetCoreCLI@2
+  displayName: Restore
+  inputs:
+    command: restore
+    projects: '**/*.csproj'
+
+- task: DotNetCoreCLI@2
+  displayName: Build
+  inputs:
+    projects: '**/*.csproj'
+    arguments: '--configuration $(BuildConfiguration)'
+
+- task: DotNetCoreCLI@2
+  displayName: Test
+  inputs:
+    command: test
+    projects: '**/*[Tt]ests/*.csproj'
+    arguments: '--configuration $(BuildConfiguration)'
+
+- task: DotNetCoreCLI@2
+  displayName: 'Publish Web'
+  inputs:
+    command: publish
+    publishWebProjects: false
+    projects: '$(Build.SourcesDirectory)\OctopusSamples.OctoPetShop.Web/OctopusSamples.OctoPetShop.Web.csproj'
+    arguments: '--configuration $(BuildConfiguration) --output $(build.artifactstagingdirectory)\output\OctoPetShop.Web\'
+    zipAfterPublish: false
+    modifyOutputPath: false
+
+- task: DotNetCoreCLI@2
+  displayName: 'Publish Product Service API'
+  inputs:
+    command: publish
+    publishWebProjects: false
+    projects: '$(Build.SourcesDirectory)\OctopusSamples.OctoPetShop.ProductService/OctopusSamples.OctoPetShop.ProductService.csproj'
+    arguments: '--configuration $(BuildConfiguration) --output $(build.artifactstagingdirectory)\output\OctoPetShop.ProductService\'
+    zipAfterPublish: false
+    modifyOutputPath: false
+
+- task: DotNetCoreCLI@2
+  displayName: 'Publish database'
+  inputs:
+    command: publish
+    publishWebProjects: false
+    projects: '$(Build.SourcesDirectory)\OctopusSamples.OctoPetShop.Database\OctopusSamples.OctoPetShop.Database.csproj'
+    arguments: '--configuration $(BuildConfiguration) --output $(build.artifactstagingdirectory)\output\OctoPetShop.Database\ --runtime win-x64'
+    zipAfterPublish: false
+    modifyOutputPath: false
+
+- task: DotNetCoreCLI@2
+  displayName: 'Publish Shopping Cart Service'
+  inputs:
+    command: publish
+    publishWebProjects: false
+    projects: '$(Build.SourcesDirectory)\OctopusSamples.OctoPetShop.ShoppingCartService\OctopusSamples.OctoPetShop.ShoppingCartService.csproj'
+    arguments: '--configuration $(BuildConfiguration) --output $(build.artifactstagingdirectory)\output\OctoPetShop.ShoppingCartService\'
+    zipAfterPublish: false
+    modifyOutputPath: false
+
+- task: octopusdeploy.octopus-deploy-build-release-tasks.octopus-pack.OctopusPack@4
+  displayName: 'Package OctopusSamples.OctoPetShop.Database'
+  inputs:
+    PackageId: OctopusSamples.OctoPetShop.Database
+    PackageFormat: Zip
+    PackageVersion: '$(Build.BuildNumber)'
+    SourcePath: '$(build.artifactstagingdirectory)\output\OctoPetShop.Database\'
+    OutputPath: '$(Build.SourcesDirectory)\output'
+
+- task: octopusdeploy.octopus-deploy-build-release-tasks.octopus-pack.OctopusPack@4
+  displayName: 'Package OctopusSamples.OctoPetShop.Web'
+  inputs:
+    PackageId: OctopusSamples.OctoPetShop.Web
+    PackageFormat: Zip
+    PackageVersion: '$(Build.BuildNumber)'
+    SourcePath: '$(build.artifactstagingdirectory)\output\OctoPetShop.Web\'
+    OutputPath: '$(Build.SourcesDirectory)\output'
+
+- task: octopusdeploy.octopus-deploy-build-release-tasks.octopus-pack.OctopusPack@4
+  displayName: 'Package OctopusSamples.OctoPetShop.ProductService'
+  inputs:
+    PackageId: OctopusSamples.OctoPetShop.ProductService
+    PackageFormat: Zip
+    PackageVersion: '$(Build.BuildNumber)'
+    SourcePath: '$(build.artifactstagingdirectory)\output\OctoPetShop.ProductService\'
+    OutputPath: '$(Build.SourcesDirectory)\output'
+
+- task: octopusdeploy.octopus-deploy-build-release-tasks.octopus-pack.OctopusPack@4
+  displayName: 'Package OctopusSamples.Octopetshop.ShoppingCartService'
+  inputs:
+    PackageId: OctopusSamples.OctoPetShop.ShoppingCartService
+    PackageFormat: Zip
+    PackageVersion: '$(Build.BuildNumber)'
+    SourcePath: '$(build.artifactstagingdirectory)\output\OctoPetShop.ShoppingCartService\'
+    OutputPath: '$(Build.SourcesDirectory)\output'
+
+- task: octopusdeploy.octopus-deploy-build-release-tasks.octopus-push.OctopusPush@4
+  displayName: 'Push Packages to Octopus'
+  inputs:
+    OctoConnectedServiceName: OctopusWebinars
+    Space: 'Spaces-222'
+    Package: '$(Build.SourcesDirectory)/output/*.zip'
+";
 
             //Act
             ConversionResponse gitHubOutput = conversion.ConvertAzurePipelineToGitHubAction(yaml);
