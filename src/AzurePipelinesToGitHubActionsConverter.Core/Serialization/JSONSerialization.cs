6e22bbb90957085811d38d0ae7c60599838c62ac
Sam Smith <samsmithnz@gmail.com>
2022-05-28 15:33:46 -0400

    Experimenting with generating all dependencies

--- src/AzurePipelinesToGitHubActionsConverter.Core/Serialization/JSONSerialization.cs
+++ src/AzurePipelinesToGitHubActionsConverter.Core/Serialization/JSONSerialization.cs
@@ -19,6 +19,19 @@ namespace AzurePipelinesToGitHubActionsConverter.Core.Serialization
             return JsonSerializer.Deserialize<JsonElement>(processedYaml);
         }
 
+        public static JsonDocument DeserializeStringToJsonDocument(string yaml)
+        {
+            //This looks inefficient, but helps to ensure the Yaml is clean and well formed
+            //first we convert the yaml to a object 
+            StringReader sr = new StringReader(yaml);
+            Deserializer deserializer = new Deserializer();
+            object yamlObject = deserializer.Deserialize(sr);
+            //then convert the object back to yaml again to format the yaml
+            string processedYaml = JsonSerializer.Serialize(yamlObject);
+            //Finally we can return a JsonElement object from the processed Yaml. 
+            return JsonSerializer.Deserialize<JsonDocument>(processedYaml);
+        }
+
         //From: https://stackoverflow.com/a/36125258/337421
         public static bool JsonCompare(this object obj, object another)
         {
---
+++ src/AzurePipelinesToGitHubActionsConverter.Tests/DependencyGenerationTest.cs
@@ -0,0 +1,54 @@
+﻿//using AzurePipelinesToGitHubActionsConverter.Core;
+//using AzurePipelinesToGitHubActionsConverter.Core.PipelinesToActionsConversion;
+//using AzurePipelinesToGitHubActionsConverter.Core.Serialization;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using System.Text.Json;
+
+//namespace AzurePipelinesToGitHubActionsConverter.Tests
+//{
+//    [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+//    [TestClass]
+//    public class DependencyGenerationTest
+//    {
+//        [TestMethod]
+//        public void GenerateAllActionsTest()
+//        {
+//            //Arrange
+//            Conversion conversion = new Conversion();
+//            string yaml = @"
+//jobs:
+//- job: Build
+//  pool:
+//    vmImage: ubuntu-latest
+//  steps:
+//  - script: echo 'hello world'";
+
+//            //Act
+//            //convert the yaml into json, it's easier to parse
+//            JsonDocument jsonDoc = null;
+//            if (yaml != null)
+//            {
+//                //Clean up the YAML to remove conditional insert statements
+//                string processedYaml = ConversionUtility.CleanYamlBeforeDeserializationV2(yaml);
+//                jsonDoc = JsonSerialization.DeserializeStringToJsonDocument(processedYaml);
+//            }
+//            if (jsonDoc != null)
+//            {
+//                yaml = YamlSerialization.SerializeYaml(jsonDoc.RootElement.ToString());
+//            }
+
+//            ConversionResponse gitHubOutput = conversion.ConvertAzurePipelineToGitHubAction(yaml);
+
+//            //Assert
+//            string expected = @"
+//";
+//            //When this test runs on a Linux runner, the YAML converter returns a slightly different result
+//            expected = UtilityTests.TrimNewLines(expected);
+//            Assert.AreEqual(expected, gitHubOutput.actionsYaml);
+//            expected = UtilityTests.TrimNewLines(expected);
+//            Assert.AreEqual(expected, gitHubOutput.actionsYaml);
+
+//        }
+
+//    }
+//}
